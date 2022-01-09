46dcc44ff6b1a7971799830d860e6d301a8f73df
Kartik <kartikbhavsar84@gmail.com>
2022-01-10 02:21:10 +0530

    Setter Getter generator tool completed

---
+++ common/src/com/kartik/bhavsar/util/SetterGetterGenerator.java
@@ -0,0 +1,72 @@
+
+package com.kartik.bhavsar.util;
+import java.lang.reflect.*;
+import java.lang.Class;
+import java.io.*;
+import java.util.*;
+
+// class Test {
+//     private int x;
+//     private int y;
+//     private String name;
+//     private String salary;
+
+//     public void setX(int x) {
+//         this.x = x;
+//     }
+
+//     public int getX() {
+//         return x;
+//     }
+// }
+
+public class SetterGetterGenerator {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Error: Not given class name");
+            System.out.println(
+                    "Usage: java -classpath path_to_jar_file;. com.kartik.bhavsar.util.SetterGetterGenerator <class_name>");
+            return;
+        }
+        // System.out.println(args[0]);
+        String className = args[0];
+        try {
+            Class c = Class.forName(className);
+            Field field[] = c.getDeclaredFields();
+            // System.out.println(field.length);
+            // for(int i=0;i<field.length;i++) System.out.println(field[i]);
+            ArrayList<String> al = new ArrayList<>();
+            String setLine = "", getLine = "";
+            for (int i = 0; i < field.length; i++) {
+                String name = "";
+                // System.out.println(field[i].getName());
+                // System.out.println(field[i].getType());
+                String tmp = field[i].getName();
+                String type = field[i].getType().getName();
+                name = tmp.substring(0, 1).toUpperCase() + tmp.substring(1);
+                setLine = "public void set" + name + "(" + type +" "+ tmp +")\n{\nthis." + tmp + "=" + tmp + ";\n}\n";
+                getLine = "public " + type + " get" + name + "()\n{\nreturn " + tmp + ";\n}\n";
+                // System.out.println(setLine);
+                // System.out.println(getLine);
+                al.add(setLine);
+                al.add(getLine);
+            }
+            File f = new File("setterGetter.txt");
+            RandomAccessFile raf = new RandomAccessFile(f, "rw");
+            raf.setLength(0);
+            Iterator<String> it = al.iterator();
+            while (it.hasNext()) {
+                raf.writeBytes(it.next());
+            }
+            raf.close();
+            System.out.println("Setter Getter methods are successfully generated for class: " + className +" in file SetterGetter.txt");
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Class Not Found: " + cnfe.getMessage());
+        } catch (NoClassDefFoundError ncdfe) {
+            System.out.println("Class Not Found: " + ncdfe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+
+    }
+}
---
+++ common/testcases/Bulb.java
@@ -0,0 +1,39 @@
+public class Bulb {
+    private int wattage;
+    private int price;
+    private String rating;
+    private String company;
+
+    public void setWattage(int wattage) {
+        this.wattage = wattage;
+    }
+
+    public int getWattage() {
+        return wattage;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setRating(java.lang.String rating) {
+        this.rating = rating;
+    }
+
+    public java.lang.String getRating() {
+        return rating;
+    }
+
+    public void setCompany(java.lang.String company) {
+        this.company = company;
+    }
+
+    public java.lang.String getCompany() {
+        return company;
+    }
+
+}
