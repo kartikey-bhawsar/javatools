8a737512f9e8ebd51517cb910a6485d9a422e762
Kartik <kartikbhavsar84@gmail.com>
2022-01-18 11:12:47 +0530

    constructor functionality added in SetterGetterGenerator

--- common/src/com/kartik/bhavsar/util/SetterGetterGenerator.java
+++ common/src/com/kartik/bhavsar/util/SetterGetterGenerator.java
@@ -1,24 +1,10 @@
 
 package com.kartik.bhavsar.util;
+
 import java.lang.reflect.*;
 import java.lang.Class;
 import java.io.*;
 import java.util.*;
-
-// class Test {
-//     private int x;
-//     private int y;
-//     private String name;
-//     private String salary;
-
-//     public void setX(int x) {
-//         this.x = x;
-//     }
-
-//     public int getX() {
-//         return x;
-//     }
-// }
 
 public class SetterGetterGenerator {
     public static void main(String[] args) {
@@ -28,14 +14,22 @@ public class SetterGetterGenerator {
                     "Usage: java -classpath path_to_jar_file;. com.kartik.bhavsar.util.SetterGetterGenerator <class_name>");
             return;
         }
-        // System.out.println(args[0]);
         String className = args[0];
         try {
             Class c = Class.forName(className);
             Field field[] = c.getDeclaredFields();
             // System.out.println(field.length);
             // for(int i=0;i<field.length;i++) System.out.println(field[i]);
+            String cls = c.getName();
+            String constructor = "public " + cls + "()\n{\n";
+            for (int i = 0; i < field.length; i++)
+            {
+                String tmp = field[i].getName();
+                constructor += tmp + " = \"\";\n";
+            }
+            constructor += "}\n";
             ArrayList<String> al = new ArrayList<>();
+            al.add(constructor);
             String setLine = "", getLine = "";
             for (int i = 0; i < field.length; i++) {
                 String name = "";
@@ -44,7 +38,8 @@ public class SetterGetterGenerator {
                 String tmp = field[i].getName();
                 String type = field[i].getType().getName();
                 name = tmp.substring(0, 1).toUpperCase() + tmp.substring(1);
-                setLine = "public void set" + name + "(" + type +" "+ tmp +")\n{\nthis." + tmp + "=" + tmp + ";\n}\n";
+                setLine = "public void set" + name + "(" + type + " " + tmp + ")\n{\nthis." + tmp + "=" + tmp
+                        + ";\n}\n";
                 getLine = "public " + type + " get" + name + "()\n{\nreturn " + tmp + ";\n}\n";
                 // System.out.println(setLine);
                 // System.out.println(getLine);
@@ -59,7 +54,8 @@ public class SetterGetterGenerator {
                 raf.writeBytes(it.next());
             }
             raf.close();
-            System.out.println("Setter Getter methods are successfully generated for class: " + className +" in file SetterGetter.txt");
+            System.out.println("Setter Getter methods are successfully generated for class: " + className
+                    + " in file SetterGetter.txt");
         } catch (ClassNotFoundException cnfe) {
             System.out.println("Class Not Found: " + cnfe.getMessage());
         } catch (NoClassDefFoundError ncdfe) {
---
+++ common/testcases/setget.java
@@ -0,0 +1,5 @@
+class Setget
+{
+int code;
+String title;
+}
